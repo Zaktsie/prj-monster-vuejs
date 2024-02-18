40fee9dea976dc21c94461f1c5d7914636b0781b
zak <tsienyanel@gmail.com>
2024-02-18 13:52:49 +0200

    added  computed poperty for  health bars and to determine if special attach can be used. Added a new section to display won or if its a draw

--- app.js
+++ app.js
@@ -8,25 +8,47 @@ const app = Vue.createApp({
       playerHP: 100,
       monsterHP: 100,
       currentRound: 0,
+      winner: null,
     };
   },
   computed: {
     mosnterBarStyles() {
+      if (this.monsterHP < 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHP + "%" };
     },
     playerBarStyles() {
+      if (this.playerHP < 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerHP + "%" };
     },
     canUseSpeacial() {
       return this.currentRound % 3 !== 0;
     },
   },
+  watch: {
+    playerHP(value) {
+      if (value <= 0 && this.monsterHP <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "monster";
+      }
+    },
+    monsterHP(value) {
+      if (value <= 0 && this.playerHP <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "player";
+      }
+    },
+  },
   methods: {
     attackMonster() {
       this.currentRound += 1;
-
       console.log("currentRound", this.currentRound);
-      const attackVal = getRandomValue(5, 15);
+      const attackVal = getRandomValue(5, 12);
       this.monsterHP -= attackVal;
       this.attackPlayer();
     },
@@ -39,6 +61,16 @@ const app = Vue.createApp({
       console.log("currentRound", this.currentRound);
       const attackVal = getRandomValue(10, 25);
       this.monsterHP -= attackVal;
+      this.attackPlayer();
+    },
+    healPlayerHP() {
+      this.currentRound += 1;
+      const healVal = getRandomValue(8, 20);
+      if (this.playerHP + healVal > 100) {
+        this.playerHP = 100;
+      } else {
+        this.playerHP += healVal;
+      }
       this.attackPlayer();
     },
   },
